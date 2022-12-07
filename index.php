90e5c18c6d1772346acb9cf7ae77408c66256b3d
TuranCSKT <turan.qarashli17@gmail.com>
2022-12-07 11:36:38 +0200

    Database to website restaurants information

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    
+    
+    <!--jQuery-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!--Bootsrap Grid System-->
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <!--Fonts-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--Created date-->
+    <meta name="date-created" content="2022-04-10">
+    <!--Author-->
+    <meta name="authors" content="Katrin Dianov, Povilas Jasvilis, Ali Jalilli, Turan Garashli, Muhammad Yousuf Farooq">
+    <!--KeyWords-->
+    <meta name="keywords" content="FOOD , Easy recipes">
+    <!--Other-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+    <meta charset="utf-8">
+    <!--Including Header+Footer-->
+      <script> 
+      $(function(){
+        $("#header").load("headfooter/header.html"); 
+        $("#footer").load("headfooter/footer.html"); 
+      });
+      </script>
+    <!--Showing Popup-->
+    <script>
+      
+
+find_element = function(id) {
+  return document.getElementById(id);
+}
+
+var show = function(id) {
+	find_element(id).style.display ='block';
+}
+var hide = function(id) {
+	find_element(id).style.display ='none';
+}
+    </script>
+    
+    <!--Filtered Search + Random Search + Side Filter Search-->
+    <script>
+      $(document).ready(function() {
+          $.getScript("/assets/js/normal-search.js");
+          $.getScript("/assets/js/random-search.js");
+          $.getScript("/assets/js/side-filters.js");
+      });
+    </script>
+
+    
+    <!--AJAX (User Panel)-->
+    <script>
+      // Get script
+      $(document).ready(function() {
+          $.getScript("/assets/js/user-panel.js");
+      });
+    </script>
+
+    <!--Showing Popup for random selection-->
+    <script>
+    find_element = function(id) {
+      return document.getElementById(id);
+    }
+    var show = function(id) {
+      find_element(id).style.display ='block';
+    }
+    var hide = function(id) {
+      find_element(id).style.display ='none';
+    }
+    </script>
+    
+    </head>
+
+<!--After page and scripts fully loaded, call main user panel function-->
+<body onload="control_user_panel()">
+  
+<div id="container">
+
+  <!--Header-->
+  <div id="header"></div>
+
+  <!--Map-->
+  <iframe src="https://my.atlistmaps.com/map/db571de2-5a75-4acf-a3f9-89735683f7a3?share=true"
+  allow="geolocation" width="100%" height="400px" frameborder="0" scrolling="no" allowfullscreen></iframe>
+
+  <!--Search box-->
+  <div class="container1">
+    <div id="form-container">
+      <i class="fa-solid fa-location-pin"></i>
+      <input type="search" id="searchbox" placeholder="In which city do you want to eat?">
+      <button class="search-button" onclick="liveSearch()">Search</button>
+      <a class="random-button" onclick="show('popup1')"><i class="fa-solid fa-dice"></i></a>
+    </div>
+  </div>
+
+  <!--Hidden Popup-->
+  <div class="popup" id="popup1">
+    <div class="survey-heading-container">
+      <h4>Survey</h4>
+      <a class="close-popup" href="#" onclick="hide('popup1')"><i class="fa-solid fa-xmark"></i></a>
+    </div>
+    <form class="randomizer_form">
+      <legend><span class="legend_number">1</span>Location</legend>
+      <label for="r_city">City</label><br>
+      <input type="text" name="r_city" id="r_city"><br><br>
+      <legend><span class="legend_number">2</span>Preferences</legend>
+      <p>Cuisine</p>
+         <input type="radio" id="Asian" name="radio_cuisine" value="Asian">
+         <label for="Asian">Asian</label><br>
+         <input type="radio" id="Italian" name="radio_cuisine" value="Italian">
+         <label for="Italian">Italian</label><br>
+         <input type="radio" id="European" name="radio_cuisine" value="European">
+         <label for="European">European</label>
+         <br> 
+         <br>
+
+      <p>Price</p>
+         <input type="radio" id="age1" name="radio_price" value="1">
+         <label for="age1">Cheap Eats</label><br>
+         <input type="radio" id="age2" name="radio_price" value="2">
+         <label for="age2">Mid-Range</label><br>  
+         <input type="radio" id="age3" name="radio_price" value="3">
+         <label for="age3">Fine Dining</label><br><br>
+
+         <a class="suggestions-button" onclick="randomSearch()">Get Suggestion <i class="fa-solid fa-dice random-dice-icon"></i></a>
+    </form>
+	</div>
+   
+
+    <!--Filter-->
+    <div id="main">
+      <div id="sidebar">
+        <h4> Filter by</h4>
+        <p>Cuisine</p>
+         <input type="radio" id="Asian" name="radio_cuisine_side" value="Asian">
+         <label for="Asian">Asian</label><br>
+         <input type="radio" id="Italian" name="radio_cuisine_side" value="Italian">
+         <label for="Italian">Italian</label><br>
+         <input type="radio" id="European" name="radio_cuisine_side" value="European">
+         <label for="European">European</label>
+        <br>  
+        <br> 
+        <p>Price</p>
+        <input type="radio" id="age1" name="radio_price_side" value="price=1">
+        <label for="age1">Cheap Eats</label><br>
+        <input type="radio" id="age2" name="radio_price_side" value="price=2">
+        <label for="age2">Mid-Range</label><br>  
+        <input type="radio" id="age3" name="radio_price_side" value="price=3">
+        <label for="age3">Fine Dining</label><br><br>
+        <input class="filter-button" type="submit" value="Apply" onclick="filterSearch()">
+      </div>
+
+
+    <!--Main Entries Container-->
+    <div id="Second">
+      <div id="Main-Entries">
+      <?php
+        include "restaurants.php";
+      ?>
+      </div>
+    </div>
+  </div>
+
+  <!--Footer-->
+  <div id="footer"></div>
+  </div>
+
+
+  
+</body>
--- restaurants.php
+++ restaurants.php
@@ -15,21 +15,54 @@
     $sql = "SELECT * FROM restaurant_info ORDER BY 'DATE' ASC"; // ASC means you're ordering from newest to oldest
     $result = $conn->query($sql);
 
+    $price_txt = "price=";
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
+            if ($row['price_range'] == 1) {
+            $actual_price = "$";
+            }
+            else if ($row['price_range'] == 2) {
+                $actual_price = "$$";
+            }
+            else{
+                $actual_price = "$$$";
+            }
 
-            echo "<a href='#'><img id='restaurant-image' src=". $row['image'] . "></a>";
-            echo "<a href='#'>" . $row['name'] . "</a>";
-            echo "<a href='#'>" . $row['cuisine'] . "</a>";
-            echo "<a href='#'>" . $row['price_range'] . "</a>";
-            echo "<a href='#'>" . $row['rating'] . "</a>";
-            echo "<a href='#'>" . $row['location'] . "</a>";
-            echo "<a href='#'>" . $row['parking'] . "</a>";
-            echo "<a href='#'>" . $row['number_tables'] . "</a>";
-            echo "<a href='#'>" . $row['contact'] . "</a>";
+            //echo "<a href='#'><img id='restaurant-image' src=". $row['image'] . "></a>";
+            //echo "<a href='#'>" . $row['name'] . "</a>";
+            //echo "<a href='#'>" . $row['cuisine'] . "</a>";
+            //echo "<a href='#'>" . $row['price_range'] . "</a>";
+            //echo "<a href='#'>" . $row['rating'] . "</a>";
+            //echo "<a href='#'>" . $row['location'] . "</a>";
+            //echo "<a href='#'>" . $row['parking'] . "</a>";
+            //echo "<a href='#'>" . $row['number_tables'] . "</a>";
+            //echo "<a href='#'>" . $row['contact'] . "</a>";
+
+        echo "<div id='Restaurant-Entry'>";
+        echo "<a href='#'><img id='restaurant-image' src=". $row['image'] . "></a>";
+        echo "<div id='res-heading-container'>";
+            echo "<h6 id='restaurant-heading'>" . $row['name'] . "</h6>";
+            echo "<a id='restaurant-price-tag'>$actual_price</a><a class='is-hidden'>$price_txt" . $row['price_range'] . "</a>";
+            echo "<a class='is-hidden'>" . $row['location'] . "</a>";
+            echo "<i class='fa-solid fa-square-parking'></i>";
+            echo "<i class='fa-solid fa-utensils'></i>";
+            
+        echo "</div>";
+          
+          echo "<a id='restaurant-cuisines'>" . $row['cuisine'] . "</a>";
+
+          echo "<span id='restaurant-stars' style='width:88%'>";
+            echo "<i class='fa fa-star' aria-hidden='true'></i>";
+            echo "<i class='fa fa-star' aria-hidden='true'></i>";
+            echo "<i class='fa fa-star' aria-hidden='true'></i>";
+            echo "<i class='fa fa-star' aria-hidden='true'></i>";
+            echo "<i class='fa fa-star' aria-hidden='true'></i>";
+         echo "</span>";
+        echo "</div>";
 
         }
     }
+    
     $conn->close();
 ?>
