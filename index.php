5a96646457767daa5ba94c0d60b7a3bac390b9d0
KINGZMON <myf11302@gmail.com>
2022-12-08 17:42:08 +0200

    reverting last commit

--- index.php
+++ index.php
@@ -20,9 +20,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width">
     <meta charset="utf-8">
-    <meta http-equiv='cache-control' content='no-cache'> 
-    <meta http-equiv='expires' content='0'> 
-    <meta http-equiv='pragma' content='no-cache'>
     <!--Including Header+Footer-->
       <script> 
       $(function(){
