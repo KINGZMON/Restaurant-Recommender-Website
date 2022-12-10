12ba0f4450370cc8f59478dad73fab5b91a6413d
katrin <katrindianov05@gmail.com>
2022-12-10 16:37:19 +0200

    Updated database

--- DB Tables/restaurant_info (1).sql
+++ DB Tables/restaurant_info (1).sql
@@ -2,8 +2,8 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Nov 27, 2022 at 11:25 AM
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 10, 2022 at 03:34 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -45,15 +45,15 @@ CREATE TABLE `restaurant_info` (
 --
 
 INSERT INTO `restaurant_info` (`id`, `name`, `image`, `cuisine`, `price_range`, `rating`, `location`, `parking`, `number_tables`, `contact`) VALUES
-(1, 'Mamma Pizza', 'https://media-cdn.tripadvisor.com/media/photo-m/1280/1e/82/44/94/mamma-gamberetto.jpg', 'Italian', 2, 4, '', 2, 54, '+37068097977'),
-(2, 'Jurgis ir Drakonas', 'https://media-cdn.tripadvisor.com/media/photo-o/0d/fc/30/f4/jd-initials.jpg', 'Italian', 2, 5, 'Kurpiu g. 26, Kaunas 44287 Lithuania', 3, 68, '+37067200320'),
-(3, 'Manami', 'https://media-cdn.tripadvisor.com/media/photo-m/1280/18/1a/80/5a/photo2jpg.jpg', 'Chinese', 2, 4, 'Islandijos pl 32 Plc Mega, Kaunas Lithuania', 1, 98, '+37068759530'),
-(4, 'Ali šokoladinė', 'https://media-cdn.tripadvisor.com/media/photo-o/12/d5/45/0d/special-desserts-in-a.jpg', 'Belgian', 3, 5, 'Laisvės al. 41, Kaunas 44309 Lithuania', 0, 78, '+37067882223'),
-(5, 'Katpedele', 'https://media-cdn.tripadvisor.com/media/photo-o/13/4e/28/42/ta-img-20180616-183621.jpg', 'European', 3, 5, 'V. Kreves pr. 57, Kaunas 50358 Lithuania', 3, 25, '+37062066323'),
+(1, 'Mamma Pizza', 'https://media-cdn.tripadvisor.com/media/photo-m/1280/1e/82/44/94/mamma-gamberetto.jpg', 'Italian', 2, 4, 'Vytauto prospektas 37, Kaunas 44352 Lithuania', 2, 54, '+37068097977'),
+(2, 'Jurgis ir Drakonas', 'https://lh3.googleusercontent.com/p/AF1QipOy1Yf2HHJNcZeJP-L6nSPRcPYJVcg83bAdSImd', 'Italian', 2, 5, 'Kurpiu g. 26, Kaunas 44287 Lithuania', 3, 68, '+37067200320'),
+(3, 'Manami', 'https://imageproxy.wolt.com/venue/5b3cd8afe8f855000cd53e0a/77647af6-6f8d-11eb-920e-4237232205eb_38.manami_menu_3.jpg', 'Chinese', 2, 4, 'Islandijos pl 32 Plc Mega, Kaunas Lithuania', 1, 98, '+37068759530'),
+(4, 'Ali šokoladinė', 'https://alisokoladine.lt/lt/themes/leo_odin/assets/img/modules/appagebuilder/icon/sokolado-myletojams.jpg', 'Belgian', 3, 5, 'Laisvės al. 41, Kaunas 44309 Lithuania', 0, 78, '+37067882223'),
+(5, 'Katpedele', 'https://imageproxy.wolt.com/venue/5bfade0baef7b2000bc9d6ae/50583dec-6f8a-11eb-805f-5e3d862d8496_35.katpedele_menu_5.jpg', 'European', 3, 5, 'V. Kreves pr. 57, Kaunas 50358 Lithuania', 3, 25, '+37062066323'),
 (6, 'Restoranas DIA', 'https://media-cdn.tripadvisor.com/media/photo-o/0f/d9/38/db/hazelnuts.jpg', 'French', 2, 5, 'Maironio G. 9, Kaunas 44298 Lithuania', 2, 36, '+37065580308'),
-(7, 'Agave', 'https://media-cdn.tripadvisor.com/media/photo-w/0f/a2/3f/ea/open-kitchen-in-agave.jpg', 'Mexican', 2, 5, 'Rotuses a. 3 Rotuses Square, Kaunas 44280 Lithuania', 2, 25, '+37061698496'),
-(8, 'Medziotoju Uzeiga', 'https://media-cdn.tripadvisor.com/media/photo-o/15/44/9b/db/interior.jpg', 'European', 2, 5, 'Rotushes a. 10, Kaunas 44279 Lithuania', 3, 91, '+37037320956'),
-(9, 'Moksha', 'https://media-cdn.tripadvisor.com/media/photo-s/02/c7/c0/cd/moksha.jpg', 'Indian', 3, 4, 'Vasario 16-osios g. 6-1, Kaunas 44250 Lithuania', 5, 47, '+370 676 71649'),
+(7, 'Agave', 'https://static.meniu.lt/media/cache/outbound_thumb_840x560/uploads/restaurant/photo/3/4621/87f65a43c9a7255cdd74a35568d7d2ed2a86cb37.jpeg', 'Mexican', 2, 5, 'Rotuses a. 3 Rotuses Square, Kaunas 44280 Lithuania', 2, 25, '+37061698496'),
+(8, 'Medziotoju Uzeiga', 'https://medziotojai.lt/wp-content/uploads/2020/11/Degustacine-5-1-scaled.jpg', 'European', 2, 5, 'Rotushes a. 10, Kaunas 44279 Lithuania', 3, 91, '+37037320956'),
+(9, 'Moksha', 'https://images.happycow.net/venues/1024/48/56/hcmp48564_403386.jpeg', 'Indian', 3, 4, 'Vasario 16-osios g. 6-1, Kaunas 44250 Lithuania', 5, 47, '+370 676 71649'),
 (10, 'Talutti - Fresh and Tasty', 'https://media-cdn.tripadvisor.com/media/photo-o/0d/3c/55/ef/pancakes.jpg', 'Italian', 2, 5, 'Taikos pr. 12, Kaunas 50191 Lithuania', 7, 58, '+37065668999');
 
 --
